912ba5730d3f5d6ba0a6612bef9e48738f70751b
Dolav Nitay <dolavn16@gmail.com>
2017-09-04 18:41:39 +0300

    Comments in Shape.h , Shape.cpp, Circle.h, Circle.cpp

--- bricksBreaking/Circle.h
+++ bricksBreaking/Circle.h
@@ -1,30 +1,118 @@
-#pragma once
+/*
+	This class represents the circle shape. It derives from the class Shape.
+
+	@author Dolav Nitay
+	@version 1.0
+*/
+
 #include "Shape.h"
 #include "Point.h"
 #include <math.h>
 #include <string>
 #include <vector>
 
-const double PI = sinh(1) * 2;
+#ifndef CIRCLE_H_
+#define CIRCLE_H_
+
+
+const double PI = sinh(1) * 2; //Using arcsin to calculate the value of PI
 
 
 class Circle :public Shape {
 public:
+	/*
+		Constructs a new circle, object.
+		
+		@param x The x coordinate of the center of the circle.
+		@param y The y coordinate of the center of the circle.
+		@param radius The radius of the circle.
+		@param c The color of the circle.
+	*/
 	Circle(int x, int y, int radius, Color c);
+
+	/*
+		Deep copies an existing circle.
+
+		@param other The other circle to be deep copied from.
+	*/
 	Circle(const Circle& other);
+
+	/*
+		Assignment operator, used to deep copy an existing circle to this one.
+	*/
 	Circle& operator=(const Circle& other);
+
+	/*
+		Destructor for this circle object.
+	*/
 	virtual ~Circle();
+
+	/*
+		Draws this circle using an SDL_Renderer
+	*/
 	void draw(SDL_Renderer& renderer);
+
+	/*
+		Sets the center of this circle.
+
+		@param x The new x coordinate of the center of this circle.
+		@param y The new y coordinate of the center of this circle.
+	*/
 	void setCenter(int x, int y);
+
+	/*
+		Returns the x coordinate of the center of this circle.
+
+		@returns The x coordinate of the center of this circle.
+	*/
 	int getX();
+
+	/*
+		Returns a pointer to a clone of this circle.
+
+		@returns A pointer to a clone of this circle.
+	*/
 	virtual Shape* clone() const;
+
+	/*
+		Returns a string representation of this circle.
+
+		@returns A string representation of this circle.
+	*/
 	virtual std::string toString() const;
 private:
-	int x;
-	int y;
-	int radius;
+	int x; //The x coordinate of the center of this circle.
+	int y; //The y coordinate of the center of this circle.
+	int radius; //The radius of this circle.
+
+	/*
+		A vector contatining pairs of points, between which the renderer will
+		draw lines when renderering this circle.
+	*/
 	std::vector<std::pair<Point, Point>> lines;
+
+	/*
+		Converts an angle from degrees to radians.
+
+		@param angle An angle.
+		@returns The angle in units of radians.
+	*/
 	double getRadians(double angle);
+
+	/*
+		Initializes this circle.
+	*/
 	void init();
+
+	/*
+		Computes the distance between two points.
+
+		@param a Point a.
+		@param b Point b
+
+		@returns The distance between point a, and point b.
+	*/
 	double dist(Point a, Point b);
 };
+
+#endif
--- bricksBreaking/Shape.cpp
+++ bricksBreaking/Shape.cpp
@@ -3,15 +3,19 @@
 
 #include <iostream>
 
+//Constructs a new shape, of color color, with a random id between 0-10000
 Shape::Shape(Color color) :color(color), id(rand()%10000) {
 }
 
+//Deep copies an existing shape, assigning a new random id between 0-10000
 Shape::Shape(const Shape& other) : color(other.color),id(rand()%10000) {
 }
 
+//Destructs this shape.
 Shape::~Shape() {
 }
 
+//Assignment operator
 Shape& Shape::operator=(const Shape& other) {
 	if (&other==this)
 		return *this;
@@ -20,10 +24,12 @@ Shape& Shape::operator=(const Shape& other) {
 	return *this;
 }
 
+//Returns the color of this shape.
 Color Shape::getColor() const {
 	return color;
 }
 
+//Returns the id of this shape
 int Shape::getId() const {
 	return id;
 }

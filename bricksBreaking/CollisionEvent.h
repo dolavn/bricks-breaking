cfc9fa2156245601d27b561a756fb3309b0de743
Tal <tal.lavon1@gmail.com>
2017-09-16 14:07:14 +0300

    CollisionEvent.cpp added

--- bricksBreaking/CollisionEvent.h
+++ bricksBreaking/CollisionEvent.h
@@ -1,23 +1,23 @@
-#pragma once
 #include "Event.h"
 #include "Collidable.h"
-using namespace Game;
 
 #ifndef COLLISIONEVENT_H_
 #define COLLISIONEVENT_H_
 
-class CollisionEvent{
-public:
-	CollisionEvent();
-	CollisionEvent(const CollisionEvent& other);
-	CollisionEvent(Collidable& obj1, Collidable& obj2);
-	CollisionEvent& operator=(const CollisionEvent& other);
-	virtual ~CollisionEvent();
-	void handle();
-	Event* clone();
-private:
-	Collidable& object1;
-	Collidable& object2;
-};
+namespace Game {
+	class CollisionEvent :public Event {
+	public:
+		CollisionEvent();
+		CollisionEvent(const CollisionEvent& other);
+		CollisionEvent(Collidable& obj1, Collidable& obj2);
+		CollisionEvent& operator=(const CollisionEvent& other);
+		virtual ~CollisionEvent();
+		void handle();
+		Event* clone();
+	private:
+		Collidable& object1;
+		Collidable& object2;
+	};
+}
 
 #endif

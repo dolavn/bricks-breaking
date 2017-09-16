39467148406c4627360f63f75af9968cca83175a
Dolav Nitay <dolavn16@gmail.com>
2017-09-16 17:29:35 +0300

    Added KeyboardListener

---
+++ bricksBreaking/KeyboardListener.h
@@ -0,0 +1,15 @@
+#ifndef KEYBOARD_LISTENER_H_
+#define KEYBOARD_LISTENER_H_
+
+namespace Engine {
+	enum Key {
+		KEY_PRESSED_UP,
+		KEY_PRESSED_DOWN,
+		KEY_PRESSED_LEFT,
+		KEY_PRESSED_RIGHT,
+		KEY_PRESSED_ENTER
+	};
+	
+}
+
+#endif

ec4f87bdf5b96088c87122882a4029006a0b6b59
Tal <tal.lavon1@gmail.com>
2017-09-16 16:03:39 +0300

    uodates collisionEvent.cpp

---
+++ bricksBreaking/CollisionEvent.cpp
@@ -0,0 +1,19 @@
+#include "CollisionEvent.h"
+using namespace Game;
+
+CollisionEvent::CollisionEvent(const CollisionEvent& other) : Event(other),object1(other.object1),object2(other.object2){
+}
+
+CollisionEvent::CollisionEvent(Collidable& obj1, Collidable& obj2) : object1(obj1),object2(obj2){
+}
+
+CollisionEvent::~CollisionEvent() {
+}
+
+CollisionEvent* CollisionEvent::clone() {
+	return new CollisionEvent(*this);
+}
+
+void CollisionEvent::handle() {
+
+}
--- bricksBreaking/CollisionEvent.h
+++ bricksBreaking/CollisionEvent.h
@@ -3,17 +3,18 @@
 
 #ifndef COLLISIONEVENT_H_
 #define COLLISIONEVENT_H_
-
+/**
+	collisionEvent heirs from Event and describes a collision 
+	between two collidable object in the game.
+*/
 namespace Game {
 	class CollisionEvent :public Event {
 	public:
-		CollisionEvent();
 		CollisionEvent(const CollisionEvent& other);
 		CollisionEvent(Collidable& obj1, Collidable& obj2);
-		CollisionEvent& operator=(const CollisionEvent& other);
 		virtual ~CollisionEvent();
+		CollisionEvent* clone();
 		void handle();
-		Event* clone();
 	private:
 		Collidable& object1;
 		Collidable& object2;

7740e80823b824f8c62b70c95ef20a3dccf3f204
Tal <tal.lavon1@gmail.com>
2017-09-21 20:03:47 +0300

    Controllable.h created

---
+++ bricksBreaking/Controllable.h
@@ -0,0 +1,19 @@
+#include "Engine.h"
+
+#ifndef CONTROLLABLE_H_
+#define CONTROLLABLE_H_
+
+namespace Engine {
+	class Controllable {
+		public:
+			/**
+			Copy constructor, deep copies another Controllable object.
+
+			@param other The other Controllable object
+			*/
+			Controllable(const Controllable& other);
+	private:
+
+	}
+};
+#endif
